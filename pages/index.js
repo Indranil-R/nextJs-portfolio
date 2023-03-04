c89cd5e06b9e9b276f2b963f566d5dd029f2839d
Indranil Roy <indranilroy2003@gmail.com>
2023-03-05 00:59:59 +0530

    Completed 🥳

--- pages/index.js
+++ pages/index.js
@@ -4,10 +4,16 @@ import Hero from "@/components/hero";
 import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
 import Contact from "@/components/contact";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Indranil&apos;s Portfolio</title>
+        <meta name="description" content="This is my page description" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Navbar />
       <Hero />
       <Skills />
