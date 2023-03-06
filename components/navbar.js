ea825a988f5005d9453f69094848acab9cea1085
Indranil Roy <87275536+Indranil-R@users.noreply.github.com>
2023-03-06 10:54:17 +0530

    fixed

--- components/navbar.js
+++ components/navbar.js
@@ -29,7 +29,7 @@ function Navbar() {
   }, []);
   return (
     <nav
-      className={`fixed top-0 left-0 right-0 z-50 md:h-24 md:py-4 w-screen overflow-hidden h-16 
+      className={`sticky mb-4 top-0 left-0 right-0 z-50 md:h-24 md:py-4 w-screen overflow-hidden h-16 
     ${hasScrolled &&
         "shadow-lg bg-white bg-opacity-90 backdrop-blur-sm backdrop-filter"
         }
